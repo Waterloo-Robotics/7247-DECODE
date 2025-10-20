39d967ff8f5abf55cb1fbc16a89a7bdface9fce6
Waterloo Robotics <ftcteam7247@gmail.com>
2025-10-20 18:48:44 -0500

    implementing module into a teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightProcessingModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightProcessingModule.java
@@ -4,14 +4,36 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
 public class LimelightProcessingModule {
 
-    public DcMotor limelight;
-    public LimelightProcessingModule(DcMotor dcMotor) {
-        this.limelight = (DcMotor)dcMotor;
-    }
+public Limelight3A limelight;
+    public LimelightProcessingModule(Limelight3A limelight)
+    {
+    this.limelight = limelight;
+    limelight.pipelineSwitch(0);
 
-    public void limelightResult(LLResult) {
+    }
+    /* ------ */
+    /* limelightResult
+    *  The goal of this function is to return the robot position
+    *  relative to the april tag.
+    * */
+    public Pose2D limelightResult() {
         LLResult llResult = limelight.getLatestResult();
+
+        if (llResult != null && llResult.isValid())
+        {
+            Pose3D robot_pose = llResult.getBotpose();
+            double x = robot_pose.getPosition().toUnit(DistanceUnit.INCH).x;
+            double y = robot_pose.getPosition().toUnit(DistanceUnit.INCH).y;
+            double rot = robot_pose.getOrientation().getYaw(AngleUnit.DEGREES);
+            return new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.DEGREES, rot);
+        }
+        return null;
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelightModuleTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+@TeleOp
+public class limelightModuleTest extends OpMode {
+
+    private Limelight3A limelight;
+    private LimelightProcessingModule llResult;
+
+    @Override
+    public void init() {
+        // Initialize the Limelight from the hardware map
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        // Create LimelightProcessingModule instance
+        llResult = new LimelightProcessingModule(limelight);
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        // Get the result from the Limelight
+        Pose2D pose = llResult.limelightResult();
+
+        if (pose != null) {
+            telemetry.addData("X (inches)", pose.getX(DistanceUnit.INCH));
+            telemetry.addData("Y (inches)", pose.getY(DistanceUnit.INCH));
+            telemetry.addData("Rotation (degrees)", pose.getHeading(AngleUnit.DEGREES));
+        } else {
+            telemetry.addData("Limelight", "No valid target");
+        }
+
+        telemetry.update();
+    }
+}
