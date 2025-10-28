d718767f8803833fb72bc024bded53969e912c3b
Waterloo Robotics <ftcteam7247@gmail.com>
2025-10-28 17:32:20 -0500

    Create HoodFlywheelTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HoodFlywheelTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class HoodFlywheelTest extends OpMode {
+    private DcMotor flywheel;
+    private Servo hood;
+    flywheelModule flywheelControl;
+    private double flywheelRPM;
+
+
+    @Override
+    public void init(){
+        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
+        hood = hardwareMap.get(Servo.class, "hood");
+        flywheelControl = new flywheelModule(flywheel);
+        flywheelRPM = 0;
+    }
+    public void init_loop() {
+    }
+
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
