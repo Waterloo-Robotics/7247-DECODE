145514f6d0a6521e7c5a0dd356b3db9c07c27ed5
NicholasRHeinz <55408394+NicholasRHeinz@users.noreply.github.com>
2025-11-03 17:39:30 -0600

    ule. Cam's downfall

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/AngleModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/AngleModule.java
@@ -6,10 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class AngleModule {
     public PIDController pid_controller;
 
-public AngleMod(void)
-{
-    this.pid_controller = new PIDController((float) 0.002, 0, 0);
-}
+    public AngleModule()
+    {
+        this.pid_controller = new PIDController((float) 0.002, 0, 0);
+    }
+
     public double set_Angle( double angle, double current_angle)
     {
 
