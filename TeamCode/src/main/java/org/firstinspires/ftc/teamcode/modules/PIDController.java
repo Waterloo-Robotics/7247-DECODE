cfb5390027051270a6118ee6cc99ffabebab5574
NicholasRHeinz <55408394+NicholasRHeinz@users.noreply.github.com>
2025-11-03 18:32:58 -0600

    Adding I and D terms to the PID controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/PIDController.java
@@ -4,45 +4,77 @@ public class PIDController {
     public double target;
     public double current;
     public double error;
+    private double last_error;
 
-    public float kP;
-    public float kI;
-    public float kD;
+    /* PID Constants */
+    private float kP;
+    private float kI;
+    private float kD;
 
+    /* Output Terms */
+    private double pTerm;
+    private double iTerm;
+    private double dTerm;
+
+    /* Configuration */
     private double tolerance;
+    private double max_output = 1;
+    private double min_output = -1;
 
-    public PIDController(float p, float i, float d)
-    {
+    /* Locals */
+    private boolean limited;
+
+    public PIDController(float p, float i, float d) {
         this.kP = p;
         this.kI = i;
         this.kD = d;
     }
 
-    public void set_target(float target)
-    {
+    public void set_target(float target) {
         this.target = target;
     }
 
-    public void set_tolerance(double tolerance)
-    {
+    public void set_tolerance(double tolerance) {
         this.tolerance = tolerance;
     }
 
-    public double update(double current_position)
-    {
-        // Calculate error
+    public double update(double current_position) {
         this.error = this.target - current_position;
 
-        double pTerm = this.error * this.kP;
-        float iTerm = 0;
-        float dTerm = 0;
+        this.pTerm = this.error * this.kP;
 
-        return pTerm + iTerm + dTerm;
+        /* Only add to the integrator if we're not saturated */
+        if (!this.limited && Math.signum(this.iTerm) == Math.Signum(this.error)) {
+            this.iTerm = iTerm + (this.error * this.kI);
+        }
+
+        this.dTerm = (this.error - this.last_error) * this.kD;
+
+        double raw_output = this.pTerm + this.iTerm + this.dTerm;
+        double bound_output;
+
+        /* Bound raw output with min and max output */
+        if (raw_output < this.min_output) {
+            this.limited = true;
+            bound_output = this.min_output;
+        } else if (raw_output > this.max_output) {
+            this.limited = true;
+            bound_output = this.max_output;
+        } else {
+            this.limited = false;
+            bound_output = raw_output;
+        }
+
+        return bound_output;
     }
 
-    public boolean at_target()
-    {
+    public boolean at_target() {
         return (Math.abs(this.error) < this.tolerance);
+    }
+
+    public void set_output_limits(double min, double max) {
+        this.min_output = min;
+        this.max_output = max;
     }
 
 }
