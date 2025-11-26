a9e4190804876daa7c21e0cf7f6513eb7a933f4e
Kyeler <kyeler872@gmail.com>
2025-11-25 23:12:57 -0600

    fuse with ll

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/FieldPositionEstimation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/FieldPositionEstimation.java
@@ -1,40 +1,83 @@
 package org.firstinspires.ftc.teamcode.modules;
 
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
 
 public class FieldPositionEstimation {
-    /* Purpose of this file
-    * The end goal is to ask this module where we are on the field.
-    * It may be using PinPoint or it may be updated with April Tag positions
-    * to "re-localize".
-    * */
+
     private GoBildaPinpointDriver pinpoint;
     private Pose2D field_position;
     public Pose2D relative_robot_position;
-    private boolean on_red_side;
+    private boolean on_red_side; // what is this for? - kyeler
+
+    private Limelight3A limelight;
+    private Pose2D limelight_position;
 
     public FieldPositionEstimation(GoBildaPinpointDriver pinpoint, boolean on_red_side)
     {
         this.pinpoint = pinpoint;
         this.pinpoint.setOffsets(155, -25, DistanceUnit.MM);
         this.on_red_side = on_red_side;
-        this.pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        this.pinpoint.setEncoderDirections(
+                GoBildaPinpointDriver.EncoderDirection.REVERSED,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD
+        );
     }
 
-    /*  update_from_pinpoint
-    *   Update relative_robot_position using the most recent data from pinpoint
-    * */
+    public void attachLimelight(Limelight3A limelight) {
+        this.limelight = limelight;
+        this.limelight.pipelineSwitch(0);
+        this.limelight.start();
+    }
+
     public void update_from_pinpoint()
     {
         pinpoint.update();
         double x = pinpoint.getPosX(DistanceUnit.INCH);
         double y = pinpoint.getPosY(DistanceUnit.INCH);
         double rot = pinpoint.getHeading(AngleUnit.DEGREES);
-        this.relative_robot_position = new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.DEGREES, rot);
+        this.relative_robot_position =
+                new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.DEGREES, rot);
+    }
+
+    public void update_from_limelight() {
+        if (this.limelight == null) return;
+
+        LLResult llResult = this.limelight.getLatestResult();
+
+        if (llResult != null && llResult.isValid()) {
+            List<LLResultTypes.FiducialResult> results = llResult.getFiducialResults();
+            if (results.isEmpty()) return;
+
+            Pose3D robotPoseField = results.get(0).getRobotPoseFieldSpace();
+            if (robotPoseField == null) return;
+
+            double x = robotPoseField.getPosition().toUnit(DistanceUnit.INCH).x;
+            double y = robotPoseField.getPosition().toUnit(DistanceUnit.INCH).y;
+            double heading = robotPoseField.getOrientation().getYaw(AngleUnit.DEGREES);
+
+            this.limelight_position =
+                    new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.DEGREES, heading);
+        }
+    }
+
+    public Pose2D getFusedFieldPosition() {
+        if (limelight_position != null) {
+            this.field_position = limelight_position;
+        } else if (relative_robot_position != null) {
+            this.field_position = relative_robot_position;
+        }
+
+        return this.field_position;
     }
 
     public void reset_pinpoint()
@@ -42,5 +85,4 @@ public class FieldPositionEstimation {
         pinpoint.resetPosAndIMU();
         pinpoint.recalibrateIMU();
     }
-
 }
