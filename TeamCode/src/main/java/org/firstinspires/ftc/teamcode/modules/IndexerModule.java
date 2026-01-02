5ad10da7d3a3ff34fef935c8ebff3e1554190522
NicholasRHeinz <55408394+NicholasRHeinz@users.noreply.github.com>
2026-01-01 20:48:07 -0600

    Working with delay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/IndexerModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/IndexerModule.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.modules;
 
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,20 +17,22 @@ public class IndexerModule {
 
     public enum ModuleStates {
         WAITING,
+        SHOOT_DELAY,
         SHOOT_INDIVIDUAL,
         SHOOT_ALL;
     }
     public ModuleStates module_state = ModuleStates.WAITING;
 
     private IndexerSpot last_fired_indexer;
+    private ElapsedTime delay = new ElapsedTime();
 
     public IndexerModule(Servo a1_servo, RevColorSensorV3 a1_color_a, RevColorSensorV3 a1_color_b,
                          Servo a2_servo, RevColorSensorV3 a2_color_a, RevColorSensorV3 a2_color_b,
                          Servo a3_servo, RevColorSensorV3 a3_color_a, RevColorSensorV3 a3_color_b)
     {
-        this.artifact_1 = new IndexerSpot(a1_servo, a1_color_a, a1_color_b, 0.9, 0, 0.78);
-        this.artifact_2 = new IndexerSpot(a2_servo, a2_color_a, a2_color_b, 0.5, 0, 0.38);
-        this.artifact_3 = new IndexerSpot(a3_servo, a3_color_a, a3_color_b, 0.4, 1, 0.52);
+        this.artifact_1 = new IndexerSpot(a1_servo, a1_color_a, a1_color_b, 1, 0, 0.76);
+        this.artifact_2 = new IndexerSpot(a2_servo, a2_color_a, a2_color_b, 1, 0, 0.73);
+        this.artifact_3 = new IndexerSpot(a3_servo, a3_color_a, a3_color_b, 0, 1, 0.14);
 
         this.artifacts.add(this.artifact_1);
         this.artifacts.add(this.artifact_2);
@@ -77,6 +80,13 @@ public class IndexerModule {
                 }
 
                 break;
+            case SHOOT_DELAY:
+                if (delay.seconds() > 0.1)
+                {
+                    this.last_fired_indexer.shoot();
+                    this.module_state = ModuleStates.SHOOT_INDIVIDUAL;
+                }
+
         }
     }
 
@@ -87,11 +97,14 @@ public class IndexerModule {
         {
             if (spot.artifact_status == ArtifactStatus.GREEN)
             {
-                spot.shoot();
 
                 if (this.num_artifacts == 2)
                 {
                     raiseEmptySpot();
+                    this.module_state = ModuleStates.SHOOT_DELAY;
+                    delay.reset();
+                } else {
+                    spot.shoot();
                 }
                 this.last_fired_indexer = spot;
                 return;
@@ -110,10 +123,13 @@ public class IndexerModule {
         {
             if (spot.artifact_status == ArtifactStatus.PURPLE)
             {
-                spot.shoot();
                 if (this.num_artifacts == 2)
                 {
                     raiseEmptySpot();
+                    this.module_state = ModuleStates.SHOOT_DELAY;
+                    delay.reset();
+                } else {
+                    spot.shoot();
                 }
 
                 this.last_fired_indexer = spot;
@@ -132,10 +148,13 @@ public class IndexerModule {
         {
             if (spot.artifact_status != ArtifactStatus.EMPTY)
             {
-                spot.shoot();
                 if (this.num_artifacts == 2)
                 {
                     raiseEmptySpot();
+                    this.module_state = ModuleStates.SHOOT_DELAY;
+                    delay.reset();
+                } else {
+                    spot.shoot();
                 }
 
                 this.last_fired_indexer = spot;
