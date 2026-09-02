dd6499c46184e63d34648f7fa537789a74226d6f
Waterloo Robotics <ftcteam7247@gmail.com>
2026-09-02 16:50:17 -0500

    Added Launcher Module

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/LauncherModule.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+//Module made to control the launcher
+public class LauncherModule 
+{
+
+}
+
+
