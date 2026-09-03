ce3dec0babc291a898c01982a0fb0bcda6789abd
Waterloo Robotics <ftcteam7247@gmail.com>
2026-09-02 19:24:59 -0500

    Added launch function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BioBuzzPrep_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BioBuzzPrep_TeleOp.java
@@ -10,8 +10,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
 import org.firstinspires.ftc.teamcode.modules.FCDrivebaseModule;
 
+import org.firstinspires.ftc.teamcode.modules.LauncherModule;
 import org.firstinspires.ftc.teamcode.modules.LimelightProcessingModule;
 import org.firstinspires.ftc.teamcode.modules.Table2D;
 import org.firstinspires.ftc.teamcode.modules.flywheelModule;
@@ -20,6 +22,9 @@ import org.firstinspires.ftc.teamcode.modules.flywheelModule;
 public class BioBuzzPrep_TeleOp extends OpMode {
 
     /* ---------- Drive Motors ---------- */
+    public DcMotor intakeMotor;
+    public DcMotor passthroughMotor1;
+    public DcMotor passthroughMotor2;
 
 
     /* ---------- Modules & Sensors ---------- */
@@ -31,7 +36,9 @@ public class BioBuzzPrep_TeleOp extends OpMode {
     @Override
     public void init() {
         /* ----- Hardware Map ----- */
-
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        passthroughMotor1 = hardwareMap.get(DcMotor.class, "passthroughMotor1");
+        passthroughMotor2 = hardwareMap.get(DcMotor.class, "passthroughMotor2");
 
         // Mecanum motor directions
 
@@ -42,13 +49,31 @@ public class BioBuzzPrep_TeleOp extends OpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
     }
-    public void start() 
-    {
+
+    public void start() {
 
     }
 
-    public void loop() 
-    {
+    public void loop() {
 
+    }
+
+    public void launcher() {
+        //Start intake while the A button on Gamepad 2 is pressed
+        if (gamepad2.a) {
+            intakeMotor.setPower(1);
+        } else {
+            intakeMotor.setPower(0);
+        }
+
+        //Start passthrough while the B button on Gamepad 2 is pressed
+        if (gamepad2.b) {
+            passthroughMotor1.setPower(1);
+            passthroughMotor2.setPower(1);
+        } else {
+            passthroughMotor1.setPower(0);
+            passthroughMotor2.setPower(0);
+        }
+        (Call_WinstonsAunt_From_Couch);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/LauncherModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/LauncherModule.java
@@ -2,10 +2,15 @@ package org.firstinspires.ftc.teamcode.modules;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 //Module made to control the launcher
-public class LauncherModule 
+public class LauncherModule
 {
+    //Start intake while the A button on Gamepad 2 is pressed
+    private int privateVariable = 0;
+    public int publicVariable = 1;
+    //Stop intake while the A button is not pressed
 
 }
 
